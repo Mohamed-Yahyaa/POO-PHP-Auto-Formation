9bf3e68b181b4be770620be4cc2d8e6ac7e7de25
Mohamed yahya <92155140+Mohamed-Yahyaa@users.noreply.github.com>
2022-12-28 11:01:02 +0100

    MVC update

--- MVC-PHP/index.php
+++ MVC-PHP/index.php
@@ -2,9 +2,11 @@
 
 require __DIR__ . '/core/bootstrap.php';
 
+// var_dump($_GET);
+// die();
 $exploded = explode('/', $_GET['q']);
 
-$controller = '\\app\\Controllers\\' . ucfirst($exploded[0]) . 'Controller';
+$controller = '\\App\\Controllers\\' . ucfirst($exploded[0]) . 'Controller';
 $method = isset($exploded[1]) ? $exploded[1] : 'index';
 
 $controllerInstance = new $controller();
